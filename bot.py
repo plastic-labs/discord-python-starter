5e5c7471ce92e7aa6d1defefad3343ba2af66c96
Vineeth Voruganti <13438633+VVoruganti@users.noreply.github.com>
2024-05-21 13:16:18 -0700

    Update Honcho version and syntax

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 import os
-from uuid import uuid1
+from dotenv import load_dotenv
 import discord
 from honcho import Honcho
+
+load_dotenv()
 
 intents = discord.Intents.default()
 intents.messages = True
@@ -10,9 +12,10 @@ intents.members = True
 
 app_name = "<YOUR APP NAME>"  # TODO replace with your app name
 
-# honcho = Honcho(app_name=app_name, base_url="http://localhost:8000") # uncomment to use local
-honcho = Honcho(app_name=app_name)  # uses demo server at https://demo.honcho.dev
-honcho.initialize()
+# honcho = Honcho(base_url="http://localhost:8000")  # uncomment to use local server
+honcho = Honcho(environment="demo")  # uses demo server at https://demo.honcho.dev
+
+app = honcho.apps.get_or_create(name=app_name)
 
 bot = discord.Bot(intents=intents)
 
@@ -31,48 +34,81 @@ async def on_member_join(member):
 @bot.event
 async def on_message(message):
     """Event that is run when a message is sent in a channel that the bot has access to"""
-
-    if message.author == bot.user or message.guild is not None:
+    if message.author == bot.user:
         # ensure the bot does not reply to itself
         return
 
+    print(message.content)
     # Get a user object for the message author
     user_id = f"discord_{str(message.author.id)}"
-    user = honcho.get_or_create_user(user_id)
+    user = honcho.apps.users.get_or_create(name=user_id, app_id=app.id)
 
     # Get the session associated with the user and location
     location_id = str(message.channel.id)  # Get the channel id for the message
 
-    sessions = list(
-        user.get_sessions_generator(location_id=location_id, is_active=True)
-    )
+    sessions = [
+        session
+        for session in honcho.apps.users.sessions.list(
+            user_id=user.id, app_id=app.id, is_active=True, location_id=location_id
+        )
+    ]
 
     if len(sessions) > 0:
         session = sessions[0]
     else:
-        session = user.create_session(location_id)
+        session = honcho.apps.users.sessions.create(
+            user_id=user.id, app_id=app.id, location_id=location_id
+        )
 
     # FIXME add logic to use session's messages
-    # history = list(session.get_messages_generator())
+    # history = honcho.apps.users.sessions.messages.list(
+    #     app_id=app.id,
+    #     user_id=user.id,
+    #     session_id=session.id,
+    # )
 
-    inp = message.content
-    session.create_message(is_user=True, content=inp)  # Add user message to session
+    # Add user message to session
+    input = message.content
+    honcho.apps.users.sessions.messages.create(
+        app_id=app.id,
+        user_id=user.id,
+        session_id=session.id,
+        content=input,
+        is_user=True,
+    )
 
     async with message.channel.typing():
         response = "<YOUR CHAT MODEL>"  # TODO reply with logic to generate a response
         await message.channel.send(response)
 
-    session.create_message(is_user=False, content=response)  # Add bot message to sesion
+    # Add bot message to session
+    honcho.apps.users.sessions.messages.create(
+        app_id=app.id,
+        user_id=user.id,
+        session_id=session.id,
+        content=response,
+        is_user=False,
+    )
 
 
 @bot.slash_command(name="restart", description="Restart the Conversation")
 async def restart(ctx):
     """Close the Session associated with a specific user and channel"""
     user_id = f"discord_{str(ctx.author.id)}"
-    user = honcho.get_or_create_user(user_id)
+    # user = honcho.get_or_create_user(user_id)
+    user = honcho.apps.users.get_or_create(name=user_id, app_id=app.id)
     location_id = str(ctx.channel_id)
-    sessions = list(user.get_sessions_generator(location_id))
-    sessions[0].close() if len(sessions) > 0 else None
+    # sessions = list(user.get_sessions_generator(location_id))
+    sessions = [
+        session
+        for session in honcho.apps.users.sessions.list(
+            user_id=user.id, app_id=app.id, is_active=True, location_id=location_id
+        )
+    ]
+    if len(sessions) > 0:
+        honcho.apps.users.sessions.delete(
+            app_id=app.id, user_id=user.id, session_id=sessions[0].id
+        )
 
     msg = (
         "Great! The conversation has been restarted. What would you like to talk about?"
